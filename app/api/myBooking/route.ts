166181a30f5e8d61da1dcc71d4796c7db0a5d5d7
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-05-26 10:06:58 +0530

    Featch: Added Myticket to show the ticket details.

---
+++ app/api/myBooking/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import  Jwt, { JsonWebTokenError }  from "jsonwebtoken";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req:NextRequest) {
+    try {
+        const authHeader = req.headers.get("authorization")
+
+        if (!authHeader) {
+            return NextResponse.json(
+                {error:"No token provided"},
+                {status: 404}
+            )
+        }
+
+        const token = authHeader.split(" ")[1]
+
+        const decode = Jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as {
+            userId: number;
+            email: string;
+        }
+
+        const bookings = await prisma.booking.findMany({
+            where: {
+                userId: decode.userId
+            },
+            include: {
+                bus: true
+            }
+        })
+
+        return NextResponse.json(
+            bookings
+        )
+
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            return NextResponse.json(
+                {error: "Invail token"},
+                {status: 401}
+            )
+        }
+    }
+
+    return NextResponse.json(
+        {error: "SOmething went wrong."},
+        {status: 500}
+    )
+}
