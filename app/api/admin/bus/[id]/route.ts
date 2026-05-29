63e083dfaa262148cee07048b04f4b93ecf201c2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-05-29 08:07:44 +0530

    Fixed: Small mistake has fixed.

--- app/api/admin/bus/[id]/route.ts
+++ app/api/admin/bus/[id]/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt, { JsonWebTokenError } from "jsonwebtoken"
 import { prisma } from "@/lib/prisma";
+import { error } from "node:console";
 
-export async function  DELETE(req:NextRequest, context: { params: Promise<{ id: string }> }
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }
 ) {
     try {
 
@@ -10,12 +11,12 @@ export async function  DELETE(req:NextRequest, context: { params: Promise<{ id: 
 
         if (!authHeader) {
             return NextResponse.json(
-                {error: "No tken provided"},
-                {status: 401}
+                { error: "No tken provided" },
+                { status: 401 }
             )
         }
 
-        const token = authHeader.split("")[1]
+        const token = authHeader.split(" ")[1]
 
         const decode = await jwt.verify(
             token,
@@ -28,8 +29,8 @@ export async function  DELETE(req:NextRequest, context: { params: Promise<{ id: 
 
         if (decode.role !== "admin") {
             return NextResponse.json(
-                {error: "Access denied, Admin only"},
-                {status: 403}
+                { error: "Access denied, Admin only" },
+                { status: 403 }
             )
         }
 
@@ -37,33 +38,46 @@ export async function  DELETE(req:NextRequest, context: { params: Promise<{ id: 
 
         const busId = Number(params.id)
 
-         if (!busId) {
+        if (!busId) {
             return NextResponse.json(
                 { error: "Invalid bus ID" },
                 { status: 400 }
             );
         }
 
-        const bus = await prisma.bus.findMany({
+        const bus = await prisma.bus.findUnique({
+            where: {
+                id: busId
+            }
+        })
+
+        if (!bus) {
+            return NextResponse.json(
+                {error: "Bus not found"},
+                {status: 404}
+            )
+        }
+
+        await prisma.bus.delete({
             where: {
                 id: busId
             }
         })
 
         return NextResponse.json(
-            {messge: "Bus deleted successully."}
+            { messge: "Bus deleted successully." }
         )
     } catch (error) {
         if (error instanceof JsonWebTokenError) {
             return NextResponse.json(
-                {error: "Token not provided"},
-                {status: 401}
+                { error: "Token not provided" },
+                { status: 401 }
             )
         }
 
         return NextResponse.json(
-            {error: "Something went wrong"},
-            {status: 500}
+            { error: "Something went wrong" },
+            { status: 500 }
         )
     }
 }
