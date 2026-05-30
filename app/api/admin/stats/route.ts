eb1ddd6ee12bc419386a4f7d897833962c628d99
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2026-05-30 10:03:59 +0530

    Fetch: Add a stats page to seat the total count of all user, bus, bookings for admin.

---
+++ app/api/admin/stats/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt, { JsonWebTokenError } from 'jsonwebtoken'
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req:NextRequest) {
+    try {
+        const authHeader = req.headers.get("authorization")
+
+        if (!authHeader) {
+            return NextResponse.json(
+                {error: "No token has provided."},
+                {status: 401}
+            )
+        }
+
+        const token = authHeader.split(" ")[1]
+
+        const decode = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as {
+            userId: number,
+            email: string,
+            role: string
+        }
+
+        if (decode.role !== "admin") {
+            return NextResponse.json(
+                {error: "Access denied, Only admin."},
+                {status: 403}
+            )
+        }
+
+        const totalUser = await prisma.user.count()
+        const totalBus = await prisma.bus.count()
+        const totalBooking = await prisma.booking.count()
+
+        return NextResponse.json({
+            totalBooking, 
+            totalBus, 
+            totalUser
+        })
+
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            return NextResponse.json(
+                {error: "Invaild token."},
+                {status: 401}
+            )
+        }
+        return NextResponse.json(
+            {error: "Somethink went wrong."},
+            {status: 500}
+        )
+    }
+}
